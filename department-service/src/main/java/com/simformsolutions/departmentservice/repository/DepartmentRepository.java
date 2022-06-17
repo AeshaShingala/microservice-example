88f1b6a37673839f4bbdf068ecce4675429c48b8
AeshaShingala <84899312+AeshaShingala@users.noreply.github.com>
2022-06-17 13:10:34 +0530

    department service
    microservice-1

---
+++ department-service/src/main/java/com/simformsolutions/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.simformsolutions.departmentservice.repository;
+
+import com.simformsolutions.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
---
+++ department-service/src/main/java/com/simformsolutions/departmentservice/service/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.simformsolutions.departmentservice.service;
+
+import com.simformsolutions.departmentservice.entity.Department;
+import com.simformsolutions.departmentservice.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department)
+    {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.findById(departmentId).orElse(null);
+    }
+}
